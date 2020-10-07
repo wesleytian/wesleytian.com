40bee3e10d2a2181154f7b01435ad2a31a8cafa4
wesleytian <tian.wesley@gmail.com>
2020-10-07 11:35:10 -0700

    fixed some writing

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,3 +1,4 @@
+import styles from '../styles/Home.module.css';
 import React from 'react';
 import { Typography } from '@material-ui/core';
 
@@ -7,11 +8,25 @@ export default function AboutMe() {
 			<Typography variant="h4" gutterBottom>
 				<b>About Me</b>
 			</Typography>
-			<Typography variant="body1" color="textSecondary" paragraph>
-				Hey, my name is Wesley Tian. I'm a software engineer and the
-				first hire at Virtually, a "Shopify for Online Schools." We
+			<Typography
+				variant="body1"
+				color="textSecondary"
+				paragraph
+				className={styles.description}
+			>
+				Hey, my name is Wesley Tian. I'm a founding member of{' '}
+				<a href="https://tryvirtually.com/">Virtually</a>, the "Shopify
+				for Online Schools". As Virtually's first hire, I help out in
+				multiple domains, but my forte is software engineering. We
 				recently went through Y Combinator's Summer 2020 batch and
-				raised a seed round.
+				raised a{' '}
+				<a
+					href="https://www.edsurge.com/news/2020-09-28-a-shopify-for-online-schools-raises-1-75m-led-by-tiger-global"
+					target="_blank"
+				>
+					seed round
+				</a>
+				.
 			</Typography>
 			<Typography variant="body1" color="textSecondary" paragraph>
 				I was born in Mountain View, California, and spent around 30% of
@@ -21,9 +36,9 @@ export default function AboutMe() {
 				Arbor, Michigan.
 			</Typography>
 			<Typography variant="body1" color="textSecondary" paragraph>
-				I enjoy reading, writing, working on side projects such as this
-				site, visiting friends in the Bay Area, hiking, and perfecting
-				my handstand in my free time.
+				I enjoy learning, writing, building side projects, visiting
+				friends in San Francisco, hiking, and working on my handstand in
+				my free time.
 			</Typography>
 		</>
 	);
--- pages/library.js
+++ pages/library.js
@@ -30,7 +30,7 @@ export default function LibraryPage() {
 						The hard part isn't finding stuff to read; it's
 						filtering it down. Thus, I've tried very hard to curate
 						this list to include only the information that I have
-						found most insightful. I find{' '}
+						found most insightful or inspiring. I find{' '}
 						<a
 							href="https://patrickcollison.com/bookshelf"
 							target="_blank"
@@ -46,49 +46,16 @@ export default function LibraryPage() {
 						paragraph
 						className={styles.description}
 					>
-						Have you read/seen something that you think I might
-						like? If so, drop me an email at{' '}
+						Have you recently read/seen something that you think I
+						might like? If so, drop me an email at{' '}
 						<a href="mailto:tian.wesley@gmail.com" target="_blank">
 							tian.wesley@gmail.com
 						</a>
 						. Credit will be given where credit's due.
 					</Typography>
-
-					{/* Section 2 */}
-					{/* <Typography variant="h6">Quotes</Typography>
+					<Typography variant="h5">Short-Form</Typography>
 					<Grid container>
 						<List>
-							<ListItem divider disableGutters>
-								<ListItemLink
-									href="https://www.navalmanack.com/almanack-of-naval-ravikant/build-or-buy-equity-in-a-business"
-									target="_blank"
-								>
-									<ListItemText
-										className={styles.description}
-										primary={`Build or Buy Equity in a Business`}
-										secondary={`Naval Ravikant`}
-									/>
-								</ListItemLink>
-							</ListItem>
-						</List>
-					</Grid> */}
-
-					{/* Section 2 */}
-					<Typography variant="h6">Short-Form</Typography>
-					<Grid container>
-						<List>
-							<ListItem divider disableGutters>
-								<ListItemLink
-									href="https://www.navalmanack.com/almanack-of-naval-ravikant/build-or-buy-equity-in-a-business"
-									target="_blank"
-								>
-									<ListItemText
-										className={styles.description}
-										primary={`Build or Buy Equity in a Business`}
-										secondary={`Naval Ravikant`}
-									/>
-								</ListItemLink>
-							</ListItem>
 							<ListItem divider disableGutters>
 								<ListItemLink
 									href="https://www.navalmanack.com/almanack-of-naval-ravikant/be-patient"
@@ -101,22 +68,32 @@ export default function LibraryPage() {
 									/>
 								</ListItemLink>
 							</ListItem>
-						</List>
-					</Grid>
-
-					{/* Section 3 */}
-					<Typography variant="h6">Long-Form</Typography>
-					<Grid container>
-						<List>
 							<ListItem divider disableGutters>
 								<ListItemLink
-									href="https://www.amazon.com/Sapiens-Humankind-Yuval-Noah-Harari/dp/0062316117/ref=sr_1_2?crid=330KK0BNZD03H&dchild=1&keywords=sapiens+a+brief+history+of+humankind&qid=1601971005&s=books&sprefix=sapiens%2Cstripbooks%2C257&sr=1-2"
+									href="https://blog.samaltman.com/how-to-be-successful"
 									target="_blank"
 								>
 									<ListItemText
 										className={styles.description}
-										primary={`Sapiens: A Brief History of Humankind`}
-										secondary={`By Yuval Noah Harari [Genres: Nonfiction, History]`}
+										primary={`How To Be Successful`}
+										secondary={`Sam Altman`}
+									/>
+								</ListItemLink>
+							</ListItem>
+						</List>
+					</Grid>
+					<Typography variant="h5">Long-Form</Typography>
+					<Grid container>
+						<List>
+							<ListItem divider disableGutters>
+								<ListItemLink
+									href="https://www.amazon.com/Zero-One-Notes-Startups-Future-ebook/dp/B00J6YBOFQ/ref=sr_1_1?crid=2G4HN36NSV6PB&dchild=1&keywords=zero+to+one&qid=1601970992&s=books&sprefix=zero+to+o%2Cstripbooks%2C239&sr=1-1"
+									target="_blank"
+								>
+									<ListItemText
+										className={styles.description}
+										primary={`Zero to One: Notes on Startups, or How to Build the Future`}
+										secondary={`By Peter Thiel, Blake Masters [Genres: Business, Nonfiction]`}
 									/>
 								</ListItemLink>
 							</ListItem>
@@ -134,23 +111,39 @@ export default function LibraryPage() {
 							</ListItem>
 							<ListItem divider disableGutters>
 								<ListItemLink
-									href="https://www.amazon.com/Zero-One-Notes-Startups-Future-ebook/dp/B00J6YBOFQ/ref=sr_1_1?crid=2G4HN36NSV6PB&dchild=1&keywords=zero+to+one&qid=1601970992&s=books&sprefix=zero+to+o%2Cstripbooks%2C239&sr=1-1"
+									href="https://www.amazon.com/Sapiens-Humankind-Yuval-Noah-Harari/dp/0062316117/ref=sr_1_2?crid=330KK0BNZD03H&dchild=1&keywords=sapiens+a+brief+history+of+humankind&qid=1601971005&s=books&sprefix=sapiens%2Cstripbooks%2C257&sr=1-2"
 									target="_blank"
 								>
 									<ListItemText
 										className={styles.description}
-										primary={`Zero to One: Notes on Startups, or How to Build the Future`}
-										secondary={`By Peter Thiel, Blake Masters [Genres: Business, Nonfiction]`}
+										primary={`Sapiens: A Brief History of Humankind`}
+										secondary={`By Yuval Noah Harari [Genres: Nonfiction, History]`}
 									/>
 								</ListItemLink>
 							</ListItem>
 						</List>
 					</Grid>
-
-					{/* Section 4 */}
-					<Typography variant="h6">Passive</Typography>
+					<Typography variant="h5">Passive</Typography>
 					<Grid container>
 						<List>
+							<ListItem divider disableGutters>
+								<ListItemLink href="https://films.nationalgeographic.com/free-solo">
+									<ListItemText
+										className={styles.description}
+										primary={`Free Solo`}
+										secondary={`By Jimmy Chin, Elizabeth Chai Vasarhelyi [Genres: Documentary]`}
+									/>
+								</ListItemLink>
+							</ListItem>
+							<ListItem divider>
+								{/* <ListItemLink href="https://www.netflix.com/title/80114855"> */}
+								<ListItemText
+									className={styles.description}
+									primary={`Mindhunter (TV Series)`}
+									secondary={`By Joe Penhall [Genres: Crime, Drama, Thriller]`}
+								/>
+								{/* </ListItemLink> */}
+							</ListItem>
 							<ListItem divider disableGutters>
 								<ListItemLink
 									href="https://www.amazon.com/Interstellar-Matthew-McConaughey/dp/B00TU9UFTS"
@@ -171,24 +164,6 @@ export default function LibraryPage() {
 									secondary={`By Vince Gilligan [Genres: Crime, Drama, Thriller]`}
 								/>
 								{/* </ListItemLink> */}
-							</ListItem>
-							<ListItem divider>
-								{/* <ListItemLink href="https://www.netflix.com/title/80114855"> */}
-								<ListItemText
-									className={styles.description}
-									primary={`Mindhunter (TV Series)`}
-									secondary={`By Joe Penhall [Genres: Crime, Drama, Thriller]`}
-								/>
-								{/* </ListItemLink> */}
-							</ListItem>
-							<ListItem divider disableGutters>
-								<ListItemLink href="https://films.nationalgeographic.com/free-solo">
-									<ListItemText
-										className={styles.description}
-										primary={`Free Solo`}
-										secondary={`By Jimmy Chin, Elizabeth Chai Vasarhelyi [Genres: Documentary]`}
-									/>
-								</ListItemLink>
 							</ListItem>
 						</List>
 					</Grid>
