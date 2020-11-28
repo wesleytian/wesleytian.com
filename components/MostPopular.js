aa441e34e837bd21b85dbaf1b27c89006688265e
wesleytian <tian.wesley@gmail.com>
2020-11-27 17:25:19 -0800

    Update most popular posts ordering and view counts

--- components/MostPopular.js
+++ components/MostPopular.js
@@ -18,26 +18,26 @@ export default function MostPopular() {
 					snippet={
 						"Read about how I went from nearly failing high school to receiving a full-time offer at Microsoft and finishing both University of Michigan's computer science and data science programs in 2.5 years while achieving University Honors."
 					}
-					views={2169}
+					views={2218}
 				/>
 				<Box my={2}>
 					<PostSnippet
-						title={
-							'I Climbed the Tallest Mountain in the Contiguous USA'
-						}
-						link={'https://wesleytian.github.io/2019/whitney/'}
-						snippet="Last year, I convinced a few friends to climb Mt. Whitney (14,505 ft.) with me. This is a story of friendship, altitude sickness, and piss-drinking (out of necessity and not for fun, obviously...)"
-						views={604}
-					/>
-				</Box>
-				<PostSnippet
 					title={'3 Reasons Why You Should Move to Reno'}
 					link={'https://wesleytian.github.io/2020/reno/'}
 					snippet={
 						"With my job and my girlfriend's schooling being completely remote, we chose to move to Reno out of all of the places in the world. Why?"
 					}
-					views={386}
+					views={807}
 				/>
+				</Box>
+				<PostSnippet
+						title={
+							'I Climbed the Tallest Mountain in the Contiguous USA'
+						}
+						link={'https://wesleytian.github.io/2019/whitney/'}
+						snippet="Last year, I convinced a few friends to climb Mt. Whitney (14,505 ft.) with me. This is a story of friendship, altitude sickness, and piss-drinking (out of necessity and not for fun, obviously...)"
+						views={623}
+					/>
 			</List>
 			<Typography
 				variant="body1"
