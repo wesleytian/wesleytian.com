a45370ac253f8072f156ebe023ac381811ae333b
wesleytian <tian.wesley@gmail.com>
2020-11-29 19:37:41 -0800

    Inc view count for posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,15 @@
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import styles from "../../styles/Home.module.css";
-import React from "react";
+import React, { useEffect } from "react";
 import { Typography, Grid } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { incrementViews } from "../../lib/firebase";
 
 export default function Post({ postData }) {
+	useEffect(() => {
+		incrementViews(postData.id);
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<main className={styles.main}>
