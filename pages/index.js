c530e97f995efe22b0cf661905aa2152b1aced50
Wesley Tian <tian.wesley@gmail.com>
2022-06-29 22:12:13 -0700

    fix up some text

--- pages/index.js
+++ pages/index.js
@@ -5,152 +5,156 @@ import { getSortedPostsData } from "../lib/posts";
 import { getViews } from "../lib/firebase";
 
 export default function IndexPage() {
-	const [views, setViews] = useState(0);
+  const [views, setViews] = useState(0);
 
-	useEffect(() => {
-		fetchViews();
-	}, []);
+  useEffect(() => {
+    fetchViews();
+  }, []);
 
-	const fetchViews = async () => {
-		const views = await getViews("gpa");
-		setViews(views);
-	};
+  const fetchViews = async () => {
+    const views = await getViews("gpa");
+    setViews(views);
+  };
 
-	return (
-		<div className={styles.container}>
-			<Head>
-				<link rel="icon" href="/favicon.ico" />
-				<meta property="og:url" content="wesleytian.com" />
-				<meta property="og:type" content="website" />
-				<meta property="og:title" content="👋🏼 Hey! I'm Wesley Tian." />
-				<meta
-					property="og:description"
-					content="Welcome to my personal site."
-				/>
-				<meta property="og:image" content={"/posts/ogimage.jpeg"} />
-			</Head>
-			<main
-				style={{
-					marginTop: "auto",
-					marginBottom: "auto",
+  return (
+    <div>
+      <Head>
+        <title>Wesley Tian</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="wesleytian.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="👋🏼 Hey! I'm Wesley Tian." />
+        <meta
+          property="og:description"
+          content="Welcome to my personal site."
+        />
+        <meta property="og:image" content={"/posts/ogimage.jpeg"} />
+      </Head>
+      <main
+        style={{
+          marginTop: "auto",
+          marginBottom: "auto",
           marginLeft: "auto",
           marginRight: "auto",
 
-          paddingTop: "8rem",
-          paddingBottom: "8rem",
-					paddingLeft: "1rem",
-					paddingRight: "1rem",
+          paddingTop: "4rem",
+          paddingBottom: "4rem",
+          paddingLeft: "1rem",
+          paddingRight: "1rem",
 
-					maxWidth: "700px"
-				}}>
-				<div>
-					<div styles={{float: "left"}}>
-						<img
-							src="/posts/me.jpeg"
-							style={{
-								width: "100px",
-								height: "100px",
-								borderRadius: "50px",
-								objectFit: "cover"
-							}}
-						/>
-						<h1 styles={{position:"relative"}}>👋🏼 Hey! I'm Wesley.</h1>
-					</div>
-					<div>
-						<div>
-							<p>
-								🌎 I'm currently living in San Francisco, but I grew up in San
-								Jose, CA; metro Portland, Oregon; Taipei, Taiwan; and Shanghai,
-								China.
-							</p>
-							<div>
-								<p>
-									🧑🏻‍💻 I spend most of my time helping build a platform for
-									purchasing fractional ownership of homes at an{" "}
-									<a
-										className=""
-										href="https://techcrunch.com/2021/11/22/fractional-lands-5-5-million-to-let-friends-and-strangers-invest-in-real-estate-together/">
-										early-stage startup
-									</a>
-									.
-								</p>
-							</div>
-							<p>
-								Previously, I helped found an{" "}
-								<a
-									className=""
-									href="https://www.edsurge.com/news/2020-09-28-a-shopify-for-online-schools-raises-1-75m-led-by-tiger-global">
-									edtech startup
-								</a>
-								, where I wrote half the codebase, led an engineering team of 4,
-								and scaled the product from 0 to over 10K users. I went through
-								Y Combinator's summer 2020 batch with the founder and we raised
-								$1.7M in funding.
-							</p>
-							<p>
-								I was also a software engineer at Microsoft and studied computer
-								science and data science at the University of Michigan.
-							</p>
-						</div>
-					</div>
-					<div>
-						<p>I sometimes post my reflections on life:</p>
-						<ul>
-							<li>
-								May, 2022 <a href="/posts/sf">I fucking love SF</a>
-							</li>
+          maxWidth: "600px"
+        }}>
+        <div>
+          <div styles={{ float: "left" }}>
+            <img
+              src="/posts/me.jpeg"
+              style={{
+                width: "100px",
+                height: "100px",
+                borderRadius: "50px",
+                objectFit: "cover"
+              }}
+            />
+            <h1 styles={{ position: "relative" }}>👋🏼 Hey! I'm Wesley.</h1>
+          </div>
+          <div>
+            <div>
+              <div>
+                <p>
+                  🧑🏻‍💻 During the day I build software to help homies buy homes together via <a
+                    className=""
+                    href="https://techcrunch.com/2021/11/22/fractional-lands-5-5-million-to-let-friends-and-strangers-invest-in-real-estate-together/">
+                    Fractional
+                  </a>
+                  .
+                </p>
+				<p>
+					In the evenings and weekends, I enjoy playing basketball, working on my handstand, reading about nuclear fusion, listening to new albums, and working on side projects.
+				</p>
+              </div>
+              <p>
+                Previously, I helped found an{" "}
+                <a
+                  className=""
+                  href="https://www.edsurge.com/news/2020-09-28-a-shopify-for-online-schools-raises-1-75m-led-by-tiger-global">
+                  edtech startup
+                </a>
+                , where I wrote half the codebase, led an engineering team of 4,
+                and scaled the product from 0 to over 10K users. I went through
+                Y Combinator's summer 2020 batch with the founder and we raised
+                $1.7M in funding.
+              </p>
+              <p>
+                I was also briefly a software engineer at Microsoft and I studied computer
+                science and data science at the University of Michigan.
+              </p>
+              <p>
+                📍 I'm currently based out of San Francisco, but I'm originally
+                from Mountain View, California and I grew up in Milpitas,
+                California; West Linn, Oregon; Taipei, Taiwan; Shanghai, China;
+                Amherst, Massachusetts; Ann Arbor, Michigan; and Reno, Nevada
+                (in that order 😆).
+              </p>
+            </div>
+          </div>
+          <div>
+            <p>✍️ I sometimes write for fun:</p>
+            <ul>
+              <li>
+                05/2022 <a href="/posts/sf">I &lt;3 SF</a>
+              </li>
 
-							<li>
-								November, 2021{" "}
-								<a href="/posts/ironman">
-									I did a 140.6-mile Ironman race (draft)
-								</a>
-							</li>
+              <li>
+                11/2021{" "}
+                <a href="/posts/ironman">
+                  The Hardest Thing I've Ever Done: Completing a 140.6-mile Ironman Triathlon Race
+                </a>
+              </li>
 
-							<li>
-								July, 2020{" "}
-								<a href="/posts/reno">Why did I move to to Reno, Nevada?</a>
-							</li>
+              <li>
+                07/2020{" "}
+                <a href="/posts/reno">Why You Should Move to Reno, Nevada</a>
+              </li>
 
-							<li>
-								August, 2019{" "}
-								<a href="/posts/whitney">
-									I convinced 3 friends to climb the tallest mountain in the
-									contiguous US with me and we had to drink piss to survive
-								</a>
-							</li>
+              <li>
+                08/2019{" "}
+                <a href="/posts/whitney">	
+                  We Climbed the Tallest Mountain in the 
+                  Contiguous US and Had To Drink Piss to Survive
+                </a>
+              </li>
 
-							<li>
-								June 2019 <a href="/posts/gpa">I nearly failed high school</a>{" "}
-								<span style={{ color: "gray" }}>(views: {views})</span>
-							</li>
-						</ul>
+              <li>
+                06/2019 <a href="/posts/gpa">How I Nearly Failed High School</a>{" "}
+                <span style={{ color: "gray" }}>(views: {views})</span>
+              </li>
+            </ul>
 
-						<p>
-							You can find some of my other pieces <a href="/posts">here</a>.
-						</p>
-					</div>
-					<div>
-						<p>
-							Follow me on Twitter
-							<a href="https://twitter.com/wesleyytian"> here</a> (I'll follow
-							you back), connect with me on LinkedIn{" "}
-							<a href="https://linkedin.com/in/wesleytian">here</a>, and email
-							me <a href="mailto:tian.wesley@gmail.com">here</a>.
-						</p>
-					</div>
-				</div>
-			</main>
-		</div>
-	);
+            <p>
+              You can find some of my other pieces <a href="/posts">here</a>.
+            </p>
+          </div>
+          <div>
+            <p>
+              You can also reach me on Twitter (<a href="https://twitter.com/wesleyytian">wesleyytian</a>),
+              LinkedIn (
+              <a href="https://linkedin.com/in/wesleytian">wesleytian</a>), or
+              via email (
+              <a href="mailto:tian.wesley@gmail.com">tian.wesley@gmail.com</a>).
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export async function getStaticProps() {
-	const allPostsData = getSortedPostsData();
+  const allPostsData = getSortedPostsData();
 
-	return {
-		props: {
-			allPostsData
-		}
-	};
+  return {
+    props: {
+      allPostsData
+    }
+  };
 }
